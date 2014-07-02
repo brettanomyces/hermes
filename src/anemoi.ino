c431ea241fcca4bf828ac5a2237dac60f22ea3d4
brettanomyces <brettyukich@gmail.com>
2014-07-02 21:21:12 +1200

    add some functions for the digital pot

--- src/anemoi.ino
+++ src/anemoi.ino
@@ -1,7 +1,17 @@
+// include the SPI library
+#include <SPI.h>
+
+// set pin 10 as the slave select for the difital pot
+const int slaveSelectPin = 10;
+const float OFFSET = 5000;
 
 void setup() {
 
   Serial.begin(9600);
+  // set the slaveSelectPin as an output
+  pinMode(slaveSelectPin, OUTPUT);
+  // initialize SPI
+  SPI.begin();
 
 } // Void Setup Close
 
@@ -31,7 +41,6 @@ void loop() {
 
 } // void loop close
 
-
 float calculateVoltage(float analog){
   return (analog / 1024) * 5.0;
 }
@@ -40,3 +49,20 @@ float calculateResistance(float voltage){
   return 100000 * (1 / ((5 / voltage) - 1));
 }
 
+int resistanceToValue(float resistance) {
+  return map(resistence, 0, 100000, 0, 257);
+}
+
+float offsetResistance(float resistance) {
+  return resistance - OFFSET;
+}
+
+void digitalPotWrite(int address, int value) {
+  // take the SS pin low to select the chip
+  digitalWrite(slaveSelectPin, LOW);
+  // send in the address and value via SPI
+  SPI.transfer(address);
+  SPI.transfer(value);
+  // take the SS pin high to de-select the chip
+  digitalWrite(slaveSelectPin, HIGH);
+}
