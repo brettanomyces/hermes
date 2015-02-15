c2d185181f29037530471d27ffe37bb7e0da2e2a
brettanomyces <brettyukich@gmail.com>
2015-02-16 12:28:49 +1300

    add delay to relays

--- src/Relay.cpp
+++ src/Relay.cpp
@@ -1,6 +1,14 @@
 #include "Relay.h"
 
-Relay::Relay(int pin){
+Relay::Relay(int pin, long delay) : m_delay(delay){
+	m_pin = pin;
+	pinMode(m_pin, OUTPUT);
+	// start in off state
+	digitalWrite(m_pin, HIGH);
+	m_on = false;
+}
+
+Relay::Relay(int pin) : m_delay(0) {
 	m_pin = pin;
 	pinMode(m_pin, OUTPUT);
 	// start in off state
@@ -9,30 +17,51 @@ Relay::Relay(int pin){
 }
 
 void Relay::on(){
-	// check that relay is not already on
-	if(!isOn()){
-		digitalWrite(m_pin, LOW);
-		Serial.print(m_pin);
-		Serial.println(" turned on");
+	if(!m_on){
+		if(m_delay.ok()){
+			digitalWrite(m_pin, LOW);
+			Serial.print(m_pin);
+			m_on = true;
+			m_delay.reset();
+			Serial.print(m_pin);
+			Serial.println(" turned on");
+		} else {
+			Serial.print(m_pin);
+			Serial.println(" waiting to turn on");
+		}
 	} else {
 		Serial.print(m_pin);
 		Serial.println(" already on");
 	}
-	m_on = true;
 }
 
 void Relay::off(){
-	if(isOn()){
-		digitalWrite(m_pin, HIGH);
-		Serial.print(m_pin);
-		Serial.println(" turned off");
+	if(m_on){
+		if(m_delay.ok()){
+			digitalWrite(m_pin, HIGH);
+			Serial.print(m_pin);
+			m_on = false;
+			m_delay.reset();
+			Serial.print(m_pin);
+			Serial.println(" turned off");
+		} else {
+			Serial.print(m_pin);
+			Serial.println(" waiting to turn off");
+		}
 	} else {
 		Serial.print(m_pin);
-		Serial.println(" already off");
+		Serial.print(" already off");
 	}
-	m_on = false;
 }
 
 bool Relay::isOn(){
 	return m_on;
+}
+
+bool Relay::waiting(){
+	if(m_delay.ok()){
+		return false;
+	} else {
+		return true;
+	}
 }
--- src/Relay.h
+++ src/Relay.h
@@ -2,17 +2,22 @@
 #define _HERMES_RELAY_
 
 #include <Arduino.h>
+#include "Delay.h"
+
 
 class Relay {
 	public:
+		Relay(int pin, long delay);
 		Relay(int pin);
 
 		void on();
 		void off();
 		bool isOn();
+		bool waiting();
 
 	private:
 		int m_pin;
 		bool m_on;
+		Delay m_delay;
 };
 #endif
