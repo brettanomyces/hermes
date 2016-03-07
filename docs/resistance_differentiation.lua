f1cd345248884002c190ff7c1713b9aab596efb5
yukichb <brett.yukich@fostermoore.co.nz>
2016-03-08 09:50:52 +1300

    get script working

--- docs/resistance_differentiation.lua
+++ docs/resistance_differentiation.lua
@@ -1,4 +1,19 @@
+#!/usr/bin/env lua
+
 -- see resistance_differentiation.jpeg for working
-max_t = 30
-min_t = 0
-print(math.sqrt(((rt(min_t) * math.pow(rt(max_t), 2)) - (rt(max_t) * math.pow(rt(min_t), 2))) / (rt(max_t) - rt(min_t))))
+t_max = 30
+t_min = 0
+
+r_25 = 5000
+
+A = -14.141963
+B = 4430.783
+C = -34078.983
+D = -8894192.9
+
+function rt(celcius)
+  local kelvin = celcius + 273
+  return math.exp(A + B / kelvin + C / math.pow(kelvin, 2) + D / math.pow(kelvin, 3)) * r_25
+end
+
+print(math.sqrt(((rt(t_min) * math.pow(rt(t_max), 2)) - (rt(t_max) * math.pow(rt(t_min), 2))) / (rt(t_max) - rt(t_min))))
