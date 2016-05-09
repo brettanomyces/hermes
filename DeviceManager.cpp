bea66f7032b833e1b2b430670e51b178c6598600
yukichb <brett.yukich@fostermoore.com>
2016-05-10 08:48:11 +1200

    remove shift register from device manager

--- DeviceManager.cpp
+++ DeviceManager.cpp
@@ -1,51 +1,14 @@
 #include "DeviceManager.h"
 
-DeviceManager::DeviceManager(int data, int latch, int clock){
-  m_data = data;
-  m_latch = latch;
-  m_clock = clock;
+DeviceManager::DeviceManager(){
 }
-
-// set all register pins to LOW
-void DeviceManager::clearRegisters(){
-  for(int i = NUM_SHIFT_REGISTER_PINS - 1; i >=  0; i--){
-    m_registers[i] = LOW;
-  }
-} 
-
-// NOTE: only call AFTER all values are set
-void DeviceManager::writeRegisters(){
-
-  digitalWrite(m_latch, LOW);
-
-  for(int i = NUM_SHIFT_REGISTER_PINS - 1; i >=  0; i--){
-    digitalWrite(m_clock, LOW);
-
-    int val = m_registers[i];
-
-    digitalWrite(m_data, val);
-    digitalWrite(m_clock, HIGH);
-
-  }
-  digitalWrite(m_latch, HIGH);
-
-}
-
-//set an individual pin HIGH or LOW
-void DeviceManager::setRegisterPin(int index, int value){
-  m_registers[index] = value;
-}
-
 
 void DeviceManager::activateRelay(int pin) {
-  setRegisterPin(pin, m_active);
-  writeRegisters();
+  digitalWrite(pin, m_active);
 }
 
 void DeviceManager::deactivateRelay(int pin) {
-  setRegisterPin(pin, !m_active);
-  writeRegisters();
-
+  digitalWrite(pin, !m_active);
 }
 
 void DeviceManager::openBaffel(int in1, int in2, int in3, int in4, int speed, int steps){
@@ -64,54 +27,51 @@ void DeviceManager::closeBaffel(int in1, int in2, int in3, int in4, int speed, i
 
 void DeviceManager::clockwise(int in1, int in2, int in3, int in4) {
   // 1
-  setRegisterPin(in1, HIGH);
-  setRegisterPin(in2, LOW);
-  setRegisterPin(in3, HIGH);
-  setRegisterPin(in4, LOW);
-  writeRegisters(); 
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+
   // 2
-  setRegisterPin(in1, LOW);
-  setRegisterPin(in2, HIGH);
-  setRegisterPin(in3, HIGH);
-  setRegisterPin(in4, LOW);
-  writeRegisters(); 
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+
   // 3
-  setRegisterPin(in1, LOW);
-  setRegisterPin(in2, HIGH);
-  setRegisterPin(in3, LOW);
-  setRegisterPin(in4, HIGH);
-  writeRegisters(); 
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+
   // 4
-  setRegisterPin(in1, HIGH);
-  setRegisterPin(in2, LOW);
-  setRegisterPin(in3, LOW);
-  setRegisterPin(in4, HIGH);
-  writeRegisters(); 
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
 }
 
 void DeviceManager::antiClockwise(int in1, int in2, int in3, int in4) {
   // 1
-  setRegisterPin(in4, HIGH);
-  setRegisterPin(in3, LOW);
-  setRegisterPin(in2, HIGH);
-  setRegisterPin(in1, LOW);
-  writeRegisters(); 
+  digitalWrite(in4, HIGH);
+  digitalWrite(in3, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in1, LOW);
+
   // 2
-  setRegisterPin(in4, LOW);
-  setRegisterPin(in3, HIGH);
-  setRegisterPin(in2, HIGH);
-  setRegisterPin(in1, LOW);
-  writeRegisters(); 
+  digitalWrite(in4, LOW);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in1, LOW);
+
   // 3
-  setRegisterPin(in4, LOW);
-  setRegisterPin(in3, HIGH);
-  setRegisterPin(in2, LOW);
-  setRegisterPin(in1, HIGH);
-  writeRegisters(); 
+  digitalWrite(in4, LOW);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in1, HIGH);
+
   // 4
-  setRegisterPin(in4, HIGH);
-  setRegisterPin(in3, LOW);
-  setRegisterPin(in2, LOW);
-  setRegisterPin(in1, HIGH);
-  writeRegisters(); 
+  digitalWrite(in4, HIGH);
+  digitalWrite(in3, LOW);
+  digitalWrite(in2, LOW);
 }
--- DeviceManager.h
+++ DeviceManager.h
@@ -1,15 +1,12 @@
 #ifndef SRC_DEVICEMANAGER_H_
 #define SRC_DEVICEMANAGER_H_
 
-// TODO use vector instead?
-#define NUM_SHIFT_REGISTERS 2
-#define NUM_SHIFT_REGISTER_PINS NUM_SHIFT_REGISTERS * 8
-
 #include "Particle.h"
+#include "Arduino.h"
 
 class DeviceManager {
   public:
-    DeviceManager(int data, int latch, int clock);
+    DeviceManager();
 
     void openBaffel(int, int, int, int, int, int);
     void closeBaffel(int, int, int, int, int, int);
@@ -17,16 +14,8 @@ class DeviceManager {
     void deactivateRelay(int);
 
   private:
-    int m_data;
-    int m_latch;
-    int m_clock;
-    boolean m_registers[NUM_SHIFT_REGISTER_PINS];
-
     int m_active = HIGH;
 
-    void writeRegisters();
-    void clearRegisters();
-    void setRegisterPin(int, int);
     void clockwise(int, int, int, int);
     void antiClockwise(int, int, int, int);
 };
