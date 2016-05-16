cd61172e616eb41d8443afbe96566f0a9152a608
yukichb <brett.yukich@fostermoore.com>
2016-05-16 13:35:36 +1200

    add unit tests for getters/setters and baffel

--- test/TemperatureController_test.cpp
+++ test/TemperatureController_test.cpp
@@ -3,7 +3,28 @@
 
 #include "TemperatureController.cpp"
 
-TEST_CASE("test shouldActivateCompressor") {
+TEST_CASE("get/set frSetTemp") {
+  TemperatureController controller;
+
+  controller.setFrSetTemp(10.0);
+  REQUIRE(controller.getFrSetTemp() == 10.0);
+}
+
+TEST_CASE("get/set fzSetTemp") {
+  TemperatureController controller;
+
+  controller.setFzSetTemp(10.0);
+  REQUIRE(controller.getFzSetTemp() == 10.0);
+}
+
+TEST_CASE("get/set difference") {
+  TemperatureController controller;
+
+  controller.setDifference(10.0);
+  REQUIRE(controller.getDifference() == 10.0);
+}
+
+TEST_CASE("shouldActivateCompressor") {
   TemperatureController controller;
 
   controller.setFzSetTemp(5.0);
@@ -39,7 +60,7 @@ TEST_CASE("test shouldActivateCompressor") {
 
 }
 
-TEST_CASE("test shouldDeactivateCompressor") {
+TEST_CASE("shouldDeactivateCompressor") {
   TemperatureController controller;
 
   controller.setFzSetTemp(5.0);
@@ -67,5 +88,59 @@ TEST_CASE("test shouldDeactivateCompressor") {
 
   SECTION("temp greater than fzSetTemp and delay active") {
     REQUIRE(controller.shouldDeactivateCompressor(7.0, true) == false);
+  }
+}
+
+TEST_CASE("shouldOpenBaffel") {
+  TemperatureController controller;
+
+  controller.setFrSetTemp(15.0);
+  controller.setDifference(0.5);
+  
+  SECTION("temp greater than frSetTemp plus diff") {
+    REQUIRE(controller.shouldOpenBaffel(20.0) == true);
+  }
+
+  SECTION("temp equal to frSetTemp plus diff") {
+    REQUIRE(controller.shouldOpenBaffel(15.5) == false);
+  }
+
+  SECTION("temp greater than frSetTemp but less than frSetTemp plus diff") {
+    REQUIRE(controller.shouldOpenBaffel(15.25) == false);
+  }
+
+  SECTION("temp equal to frSetTemp") {
+    REQUIRE(controller.shouldOpenBaffel(15.00) == false);
+  }
+
+  SECTION("temp less than frSetTemp") {
+    REQUIRE(controller.shouldOpenBaffel(10.00) == false);
+  }
+}
+
+TEST_CASE("shouldCloseBaffel") {
+  TemperatureController controller;
+
+  controller.setFrSetTemp(15.0);
+  controller.setDifference(0.5);
+  
+  SECTION("temp less than frSetTemp minus diff") {
+    REQUIRE(controller.shouldCloseBaffel(10.0) == true);
+  }
+
+  SECTION("temp equal to frSetTemp minus diff") {
+    REQUIRE(controller.shouldCloseBaffel(14.5) == false);
+  }
+
+  SECTION("temp less than frSetTemp but greater than frSetTemp plus diff") {
+    REQUIRE(controller.shouldCloseBaffel(14.75) == false);
+  }
+
+  SECTION("temp equal to frSetTemp") {
+    REQUIRE(controller.shouldCloseBaffel(15.00) == false);
+  }
+
+  SECTION("temp greater than frSetTemp") {
+    REQUIRE(controller.shouldCloseBaffel(20.00) == false);
   }
 }
