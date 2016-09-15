93ce993798e7de5008e588e3f20ea8c099109a50
Brett Yukich <brett.yukich@fostermoore.com>
2016-09-15 14:04:21 +1200

    display results immediately

--- scripts/status.sh
+++ scripts/status.sh
@@ -5,29 +5,31 @@ DEVICE=2a003a000347343337373738
 URL=https://api.particle.io/v1
 
 FZ_TEMP=$(curl --silent "$URL/devices/$DEVICE/fzTemp?access_token=$ACCESS_TOKEN" | jq .result)
-FZ_SET=$(curl --silent "$URL/devices/$DEVICE/fzSet?access_token=$ACCESS_TOKEN" | jq .result)
-FR_TEMP=$(curl --silent "$URL/devices/$DEVICE/frTemp?access_token=$ACCESS_TOKEN" | jq .result)
-FR_SET=$(curl --silent "$URL/devices/$DEVICE/frSet?access_token=$ACCESS_TOKEN" | jq .result)
-BAFFEL_OPEN=$(curl --silent "$URL/devices/$DEVICE/baffelOpen?access_token=$ACCESS_TOKEN" | jq .result)
-COMP_ACTIVE=$(curl --silent "$URL/devices/$DEVICE/compActive?access_token=$ACCESS_TOKEN" | jq .result)
-COMP_WAIT=$(curl --silent "$URL/devices/$DEVICE/compWait?access_token=$ACCESS_TOKEN" | jq .result)
-FAN_ACTIVE=$(curl --silent "$URL/devices/$DEVICE/fanActive?access_token=$ACCESS_TOKEN" | jq .result)
-HEAT_ACTIVE=$(curl --silent "$URL/devices/$DEVICE/heatActive?access_token=$ACCESS_TOKEN" | jq .result)
-HEAT_WAIT=$(curl --silent "$URL/devices/$DEVICE/heatWait?access_token=$ACCESS_TOKEN" | jq .result)
-
-printf "Freezer Set: %s\n" $FZ_SET
 printf "Freezer Temp: %s\n" $FZ_TEMP
 
-printf "Fridge Set: %s\n" $FR_SET
+FZ_SET=$(curl --silent "$URL/devices/$DEVICE/fzSet?access_token=$ACCESS_TOKEN" | jq .result)
+printf "Freezer Set: %s\n" $FZ_SET
+
+FR_TEMP=$(curl --silent "$URL/devices/$DEVICE/frTemp?access_token=$ACCESS_TOKEN" | jq .result)
 printf "Fridge Temp: %s\n" $FR_TEMP
 
+FR_SET=$(curl --silent "$URL/devices/$DEVICE/frSet?access_token=$ACCESS_TOKEN" | jq .result)
+printf "Fridge Set: %s\n" $FR_SET
+
+BAFFEL_OPEN=$(curl --silent "$URL/devices/$DEVICE/baffelOpen?access_token=$ACCESS_TOKEN" | jq .result)
 printf "Baffel Open: %s\n" $BAFFEL_OPEN
 
+COMP_ACTIVE=$(curl --silent "$URL/devices/$DEVICE/compActive?access_token=$ACCESS_TOKEN" | jq .result)
 printf "Compressor Active: %s\n" $COMP_ACTIVE
+
+COMP_WAIT=$(curl --silent "$URL/devices/$DEVICE/compWait?access_token=$ACCESS_TOKEN" | jq .result)
 printf "Compressor Waiting: %s\n" $COMP_WAIT
 
+FAN_ACTIVE=$(curl --silent "$URL/devices/$DEVICE/fanActive?access_token=$ACCESS_TOKEN" | jq .result)
 printf "Fan Active: %s\n" $FAN_ACTIVE
 
+HEAT_ACTIVE=$(curl --silent "$URL/devices/$DEVICE/heatActive?access_token=$ACCESS_TOKEN" | jq .result)
 printf "Heat Active: %s\n" $HEAT_ACTIVE
-printf "Heat Waiting: %s\n" $HEAT_WAIT
 
+HEAT_WAIT=$(curl --silent "$URL/devices/$DEVICE/heatWait?access_token=$ACCESS_TOKEN" | jq .result)
+printf "Heat Waiting: %s\n" $HEAT_WAIT
