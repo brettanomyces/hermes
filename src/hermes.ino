a7a60bfd35f0409cc2d8c3c142927bd1506c847f
Brett Yukich <brett@prenohq.com>
2017-08-10 13:31:01 +1200

    Replace thermistors with DS18B20 temperature sensors
    The DS18B20 has the following advantages:
    * Existing libraries
    * Waterproof, which makes for easier calibration using ice water
    * Easy to add additional sensors

--- src/hermes.ino
+++ src/hermes.ino
@@ -1,14 +1,17 @@
-#include "Arduino.h"
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
 #include "Baffel.h"
 #include "Delay.h"
 #include "DeviceManager.h"
 #include "DoEvery.h"
 #include "Relay.h"
 #include "TemperatureController.h"
-#include "TemperatureSensor.h"
 
-int FREEZER_SENSOR_PIN = 36;
-int FRIDGE_SENSOR_PIN = 39;
+int ONE_WIRE_SENSOR_PIN = 14;
+DeviceAddress frSensor1Address = { 0x28, 0xFF, 0xA5, 0xA0, 0x68, 0x14, 0x04, 0x36 };  // sensor #1
+DeviceAddress frSensor2Address = { 0x28, 0xFF, 0xA5, 0xA0, 0x68, 0x14, 0x04, 0x36 };  // sensor #2
+DeviceAddress fzSensorAddress = { 0x28, 0xFF, 0xA5, 0xA0, 0x68, 0x14, 0x04, 0x36 };  // sensor #3
 
 int IN1 = 34;
 int IN2 = 35;
@@ -19,9 +22,6 @@ int FAN_PIN = 25;
 int HEATER_PIN = 26;
 int COMP_PIN = 27;
 
-double ADC_STEPS = 4096;
-double V_DIVIDER_V_IN = 3.3;
-
 // other constants
 int UPDATE_PERIOD = 10000;  // 10 seconds
 double COMP_DELAY = 300000;  // 5 minutes
@@ -31,33 +31,17 @@ double HEATER_DELAY = 0;
 double DEFAULT_FR_TEMP = 19.0;
 double DEFAULT_FZ_TEMP = 4.0;
 
-int V_DIVIDER_R1 = 10000;
-int V_DIVIDER_THERMISTOR_POSITION = 2;
-
 // baffel
 int STEPPER_SPEED = 5;  // found via trial and error
 int STEPPER_STEPS = 450;  // found via trial and error
 
 int RELAY_ACTIVE_LOW = true;
 
+OneWire oneWire(ONE_WIRE_SENSOR_PIN);
+DallasTemperature temperatureSensors(&oneWire);
+
 DeviceManager deviceManager;
 DoEvery updateTimer(UPDATE_PERIOD, &deviceManager);
-
-TemperatureSensor fridgeSensor(
-  FRIDGE_SENSOR_PIN,
-  V_DIVIDER_THERMISTOR_POSITION,
-  V_DIVIDER_R1,
-  V_DIVIDER_V_IN,
-  ADC_STEPS,
-  &deviceManager);
-
-TemperatureSensor freezerSensor(
-  FREEZER_SENSOR_PIN,
-  V_DIVIDER_THERMISTOR_POSITION,
-  V_DIVIDER_R1,
-  V_DIVIDER_V_IN,
-  ADC_STEPS,
-  &deviceManager);
 
 Baffel baffel(IN1, IN2, IN3, IN4, STEPPER_STEPS, STEPPER_SPEED, &deviceManager);
 Relay compressor(COMP_PIN, COMP_DELAY, RELAY_ACTIVE_LOW, &deviceManager);
@@ -66,24 +50,13 @@ Relay heater(HEATER_PIN, HEATER_DELAY, RELAY_ACTIVE_LOW, &deviceManager);
 
 TemperatureController controller;
 
-bool deviceStateSet = false;
-
 double frSet = DEFAULT_FR_TEMP;
 double frTemp = DEFAULT_FR_TEMP;
 double fzSet = DEFAULT_FZ_TEMP;
 double fzTemp = DEFAULT_FZ_TEMP;
-bool compActive = false;
-bool compWait = false;
-bool baffelOpen = false;
-bool fanActive = false;
-bool heatActive = false;
-bool heatWait = false;
 
 void setup() {
   Serial.begin(115200);
-
-  pinMode(FRIDGE_SENSOR_PIN, INPUT);
-  pinMode(FREEZER_SENSOR_PIN, INPUT);
 
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
@@ -100,23 +73,23 @@ void setup() {
   // init timers
   updateTimer.reset();
 
-  // set temps
+  // set default temps
   controller.setFzSetTemp(DEFAULT_FZ_TEMP);
   controller.setFrSetTemp(DEFAULT_FR_TEMP);
+   
+  temperatureSensors.begin();
+  baffel.close();
+  compressor.deactivate();
+  fan.deactivate();
+  heater.deactivate();
 }
 
 void loop() {
-  if(!deviceStateSet){
-    baffel.close();
-    compressor.deactivate();
-    fan.deactivate();
-    heater.deactivate();
-    deviceStateSet = true;
-  }
-
   if (updateTimer.check()) {
-    frTemp = fridgeSensor.readTemperature();
-    fzTemp = freezerSensor.readTemperature();
+    temperatureSensors.requestTemperatures();
+
+    frTemp = temperatureSensors.getTempC(frSensor1Address);
+    fzTemp = temperatureSensors.getTempC(fzSensorAddress);
 
     // Arduino cannot sprintf floats
     Serial.print("{\"frTemp\":");
@@ -177,17 +150,5 @@ void loop() {
       }
     }
     Serial.println(message);
-
-    // update variable
-    frSet = controller.getFrSetTemp();
-    frTemp = fridgeSensor.readTemperature();
-    fzSet = controller.getFzSetTemp();
-    fzTemp = freezerSensor.readTemperature();
-    baffelOpen = baffel.isOpen();
-    compActive = compressor.isActive();
-    compWait = compressor.isWaiting();
-    fanActive = fan.isActive();
-    heatActive = heater.isActive();
-    heatWait = heater.isWaiting();
   }
 }
